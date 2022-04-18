aea31a5a66ce6fa852e7805202973ef123d3d89d
kaitotakagi <kairinotomatu@gmail.com>
2022-04-18 14:07:13 +0900

    プロジェクトのコミット

---
+++ fbx/FbxLoader.cpp
@@ -0,0 +1,28 @@
+﻿#include "FbxLoader.h"
+#include <cassert>
+
+void FbxLoader::Initialize(ID3D12Device* device)
+{
+    assert(fbxManager == nullptr);
+
+    this->device = device;
+
+    fbxManager = FbxManager::Create();
+
+    FbxIOSettings* ios = FbxIOSettings::Create(fbxManager, IOSROOT);
+    fbxManager->SetIOSettings(ios);
+
+    fbxImporter = FbxImporter::Create(fbxManager, "");
+}
+
+void FbxLoader::Finalize()
+{
+    fbxImporter->Destroy();
+    fbxManager->Destroy();
+}
+
+FbxLoader* FbxLoader::GetInstance()
+{
+    static FbxLoader instance;
+    return &instance;
+}
---
+++ fbx/FbxLoader.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "fbxsdk.h"
+
+#include <d3d12.h>
+#include <d3dx12.h>
+
+class FbxLoader
+{
+public:
+	/// <summary>
+	/// シングルトンインスタンスの取得
+	/// </summary>
+	/// <returns>インスタンス</returns>
+	void Initialize(ID3D12Device* device);
+
+	void Finalize();
+
+	static FbxLoader* GetInstance();
+
+private:
+
+	ID3D12Device* device = nullptr;
+
+	FbxManager* fbxManager = nullptr;
+
+	FbxImporter* fbxImporter = nullptr;
+	// コンストラクタ
+	FbxLoader() = default;
+	// デストラクタ
+	~FbxLoader() = default;
+	// コピーコンストラクタを禁止
+	FbxLoader(const FbxLoader& obj) = delete;
+	// コピー代入演算子を禁止
+	void operator=(const FbxLoader& obj) = delete;
+};
